2940ca8c9edda705b48e6baf236f743d7800cb6e
antiquark007 <141497727+antiquark007@users.noreply.github.com>
2025-06-09 19:10:10 +0530

    completed asssembly

--- alloc/heap.asm
+++ alloc/heap.asm
@@ -2,6 +2,9 @@ bits 32
 global memspace
 %define Heapsize (1024*1024*1024/4)
 
+Section .data alloc noexec write progbits
+    memspace:
+        dd _memspace
 
 Section .heap alloc noexec write nobits
 memspace:
