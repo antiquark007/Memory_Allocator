2c910152d677809a985f1b8c67c3515bdced20eb
antiquark007 <141497727+antiquark007@users.noreply.github.com>
2025-06-09 19:24:09 +0530

    words logic

--- alloc/alloc.c
+++ alloc/alloc.c
@@ -1,12 +1,33 @@
-//alloc.c
+// alloc.c
 #include "alloc.h"
 
-extern heap *memspace;//define it 1gb memoey
+extern heap *memspace; // define it 1gb memoey
 
-int main(int argc,char *argv[]){
-    int8 *p;
+// 2*4=8 for 7=>7/4=1+1=8/4
+void *alloc(int32 bytes)
+{
+    word words;
+    header *hdr;
+    void *mem;
 
-    p=memspace;
-    *p='X';
+    words = (!(bytes % 4)) ? bytes / 4 : (bytes / 4) + 1;
+
+    mem = $v memspace;
+    hdr = $h mem;
+    (!(hdr->w)) ? ({
+        printf("empty\n");
+        exit(0);
+    })
+                : ({
+                      printf("bla\n");
+                      exit(0);
+                  });
+
+    return $v 0;
+}
+int main(int argc, char *argv[])
+{
+    //$l memspace=l;
+    alloc(7);
     return 0;
 }
